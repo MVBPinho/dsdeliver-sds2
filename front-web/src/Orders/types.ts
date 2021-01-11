c0e4ab0a1ab72dabb3d7fb0cdcfd34f1d99a8b43
PINHO <marcos.vb.pinho@Live.com>
2021-01-10 21:06:35 -0300

    Search order location

---
+++ front-web/src/Orders/types.ts
@@ -0,0 +1,13 @@
+export type Product = {
+    id: number;
+    name: string;
+    price: number;
+    drescription: string;
+    imageUri: string;
+}
+
+export type OrderLocationdata = {
+    latitude: number;
+    longitude: number;
+    address: string;
+}
